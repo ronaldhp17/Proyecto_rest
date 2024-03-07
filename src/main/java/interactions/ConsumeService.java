31f07ee62e4ccc79a15633d65c862ad8707beab6
Ronald David Herazo Parra <rherazop@choucairtesting.com>
2024-03-07 17:45:41 -0500

    actualización build

--- src/main/java/interactions/ConsumeService.java
+++ src/main/java/interactions/ConsumeService.java
@@ -3,14 +3,18 @@ package interactions;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.Actor;
 
 public class ConsumeService {
+
+    private ConsumeService() {
+    }
 
     public static WithPost withPost(String body) {
         return Tasks.instrumented(WithPost.class, body);
     }
 
     public static WithPut withPut(String body) {
-        return Tasks.instrumented(WithPut.class,body);
+        return Tasks.instrumented(WithPut.class, body);
     }
 }
